a210d04fd0f31179f1e4c64e3b7389b0b925b7ad
Milheto <Thales.nocchi@gmail.com>
2023-03-03 16:59:07 -0300

    Create card-slider.js

---
+++ assets/scripts/card-slider.js
@@ -0,0 +1,10 @@
+var elem = document.querySelector(".main-carousel");
+var flkty = new Flickity(elem, {
+  cellAlign: "center",
+  contain: true,
+  imagesLoaded: true,
+  freeScroll: false,
+  fade: true,
+  draggable: false,
+  wrapAround: true,
+});
